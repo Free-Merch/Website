c273a61c2a7fface5e6a2308f18572d0f50a80c5
nonseodion <nonseifebhor@gmail.com>
2022-08-08 23:33:45 +0100

    fixed padding

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -58,7 +58,7 @@ const Projects = () => {
 }
 
 const NewComponent = () => (
-  <Layout className="h-full bg-grey-100 dark:bg-blue-900 py-10 px-10 text-sm md:px-24 text-grey-300 dark:text-grey-400 overflow-y-hidden">
+  <Layout className="h-full bg-grey-100 dark:bg-blue-900 py-10 px-[12px] text-sm md:px-24 text-grey-300 dark:text-grey-400 overflow-y-hidden">
     <Projects />
   </Layout>
 );
