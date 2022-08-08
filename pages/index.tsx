ab5a81690a6f81761974562735f1c15b0abdbab5
nonseodion <nonseifebhor@gmail.com>
2022-08-08 22:52:12 +0100

    added gap

--- pages/index.tsx
+++ pages/index.tsx
@@ -176,7 +176,7 @@ const Home: NextPage = () => {
           </p>
         </div>
 
-        <div className="flex flex-wrap justify-center">
+        <div className="flex flex-wrap gap-2 justify-center">
           {ProjectCards}
         </div>
         <p className="text-center mt-20">
