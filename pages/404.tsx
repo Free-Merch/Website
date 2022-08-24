b88d9be7b5e9e7b7f4f0bc08093832a2a5c431ff
nonseodion <nonseifebhor@gmail.com>
2022-08-24 07:54:46 +0100

    chnged 404 ui

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,15 +1,34 @@
 import Layout from "../components/layout";
 import Logo from "../assets/pictures/logo.png"
 import Image from "next/image";
-import { GreenButton } from "../components/buttons";
+import useBrands from "../hooks/useBrands";
+import { ProjectCard } from "../components/cards/project-card";
+import {HiOutlineExclamation} from "react-icons/hi";
 
 const CatchPage = () => {
+  let brands = useBrands()
+  brands = brands?.slice(0, 3);
+  const BrandCards = brands?.map((brand, index) => {
+  const { about, logo, campaigns, brand: name, logoBgColor, id } = brand;
+    return <ProjectCard  
+        brand={name}
+        image={logo}
+        about={about}
+        bgColor={logoBgColor}
+        campaigns={campaigns}
+        id={id}
+        key={index}
+      />
+  })
 
-  return <div className="text-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+  return <div className="text-center">
     <Image src={Logo} width={24} height={24} alt="free merch logo" />
-    <p className="mt-[20px] mb-[14px] leading-[76px] text-[60px] font-semibold">404</p>
-    <p className="mb-[45px] text-lg">This Page is outside the server </p>
-    <GreenButton href="/">Back Home</GreenButton>
+    <div className="mt-2 flex max-w-fit px-[17px] py-[3px] items-center mx-auto bg-[#FEF2F2] rounded-md text-[#991B1B] justify-center"><HiOutlineExclamation /> &nbsp;  404 Error</div>
+    <h3 className="mt-[13px] text-3xl dark:text-white text-[#191D23] font-semibold ">Page not Found</h3>
+    <p className="mb-[45px] mt-[26px] text-lg dark:text-white text-[#64748B]">Sorry, the page you are looking for doesn’t exist or has been removed. Keep exploring out site: </p>
+    <div className="child:m-3 flex flex-wrap justify-center mb-10">
+      {BrandCards}
+    </div>
   </div>
 }
 
@@ -19,7 +38,7 @@ const NewComponent = () => (
     flex-1 bg-grey-100 dark:bg-blue-900 
     py-10 px-10 md:px-24
     text-blue-400 dark:text-white overflow-y-hidden"
-    mainClass="flex flex-col"
+
   >
     <CatchPage />
   </Layout>
