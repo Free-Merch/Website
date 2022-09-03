41e42c3af3aee986341a70f2cd243ff37604edc5
nonseodion <nonseifebhor@gmail.com>
2022-09-03 14:16:31 +0100

    added gtm manually

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} >
@@ -9,6 +10,15 @@ function MyApp({ Component, pageProps }: AppProps) {
       <meta name="description" content="Get Free Merch" />
       <link rel="icon" href="favicon.ico" />
     </Head>
+    <Script id="google-tag-manager" strategy="afterInteractive">
+      {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-5RWRHZ2');
+      `}
+    </Script>
   </Component>
 }
 
