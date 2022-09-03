0b9be9d94655916a8174316af4e78bd1d0cc715f
nonseodion <nonseifebhor@gmail.com>
2022-09-03 17:52:05 +0100

    added google analytics tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,30 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} >
+  return <>
+    <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+
+    <Script strategy="lazyOnload" id='analytics script'>
+      {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+        page_path: window.location.pathname,
+        });
+      `}
+    </Script>
+    
     <Head>
       <title>FreeMerch</title>
       <meta name="description" content="Get Free Merch" />
       <link rel="icon" href="favicon.ico" />
     </Head>
-  </Component>
+  <Component {...pageProps} />
+  </>
 }
 
 export default MyApp;
