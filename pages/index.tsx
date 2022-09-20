7d6d32f2dba9b66ef316f2e41bc598d2cc7128a4
nonseodion <nonseifebhor@gmail.com>
2022-09-20 07:08:47 +0100

    testing twitter image

--- pages/index.tsx
+++ pages/index.tsx
@@ -218,7 +218,7 @@ const Home: NextPage = () => {
         <meta name="twitter:description" content="Win free branded merchandise when you join campaings & 
           promote your brand when you create campaigns." 
         />
-        <meta name="twitter:image" content="https://res.cloudinary.com/nonseodion/image/upload/v1660579251/logo_7c91fc5575.svg" />
+        <meta name="twitter:image" content="https://res.cloudinary.com/freemerchcloudinary/image/upload/v1661498127/Illustration_dark_nsqxhu.svg" />
       </Head>
 
       {/* bg ellipses */}
